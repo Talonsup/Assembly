4cffdb3567097f2c199245e9b0780039286bd6e7
Talonsup <55650863+Talonsup@users.noreply.github.com>
2024-05-27 14:48:52 +0800

    Update Experiment8.asm

--- Chapter Nine/Experiment8.asm
+++ Chapter Nine/Experiment8.asm
@@ -1,48 +1,27 @@
-assume cs:code
-
-code segment
-
-start:	mov ax,2000h
-		mov ds,ax
-		mov bx,0
-	s:	mov cl,[bx]
-		mov ch,0
-		inc cx
-		inc bx	
-		loop s
-	ok:	dec bx
-		mov dx,bx
-		mov ax,4c00h
-		int 21h
-		
-code ends
-
-end start
-
 assume cs:codesg
 
 codesg segment
 
-		mov ax,4c00h
-		int 21h
+	mov ax,4c00h
+	int 21h
 		
 start: 	mov ax,0
-	s: 	nop
-		nop
+s: 	nop
+	nop
 		
-		mov di,offset s
-		mov si,offset s2
-		mov ax,cs:[si]
-		mov cs:[di],ax
+	mov di,offset s
+	mov si,offset s2
+	mov ax,cs:[si]
+	mov cs:[di],ax
 		
-	s0: jmp short s
+s0: 	jmp short s
 	
-	s1: mov ax,0
-		int 21h
-		mov ax,0
+s1: 	mov ax,0
+	int 21h
+	mov ax,0
 		
-	s2: jmp short s1
-		nop
+s2: 	jmp short s1
+	nop
 		
 codesg ends
 end start
