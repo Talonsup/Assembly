7085197049297c049e4e9f5e007b4f591c69daca
Talonsup <55650863+Talonsup@users.noreply.github.com>
2024-05-27 18:39:13 +0800

    Create Experiment9.asm

---
+++ Chapter Nine/Experiment9.asm
@@ -0,0 +1,41 @@
+assume cs:codesg
+
+datasg segment
+	db 'welcome to masm!'
+datasg ends
+
+codesg segment
+
+start:	mov ax,datasg
+		mov ds,ax
+		
+		mov ax,0B872H	;控制显示在中间
+		mov es,ax
+		
+		mov cx,16
+		mov bx,0		;控制字符循环
+		mov si,0		;控制字符写入
+		mov di,1		;控制颜色写入
+	s:	mov al,ds:[bx]
+		mov es:[si],al
+		mov es:0a0h[si],al	;换行写入
+		mov es:140h[si],al	;换行写入	
+		
+		mov al,02h
+		mov es:[di],al
+		mov al,24h
+		mov es:0a0h[di],al
+		mov al,71h
+		mov es:140h[di],al
+		
+		inc bx
+		add si,2
+		add di,2
+		loop s
+		
+		
+		mov ax,4c00h
+		int 21h
+		
+codesg ends
+end start
